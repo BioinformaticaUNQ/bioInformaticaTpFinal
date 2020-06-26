8553b814fe63f49607d430b0c7b34a6b7fbe4a0f
lucio <lucio.francioni@xappia.com>
2020-06-26 15:37:15 -0300

    cambio a prod

--- TpFinalBio/settings/production.py
+++ TpFinalBio/settings/production.py
@@ -10,7 +10,11 @@ ALLOWED_HOSTS = ['tpbioinformatica.herokuapp.com']
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd8n2eegbi42sg0',
+        'USER': 'wxnvmwiigwlguq',
+        'PASSWORD': '1be340a2e4b24b2cdd77f0878c3d163cb0e323157c2842e878bd1cc1af1e9024',
+        'HOST': 'ec2-52-72-65-76.compute-1.amazonaws.com' ,
+        'PORT': 5432,
     }
 }
