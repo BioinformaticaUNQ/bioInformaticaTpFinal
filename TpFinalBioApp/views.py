b09e63656c1288d83fdfbe700f21c75830ebe943
lucio <franciolucio@gmail.com>
2020-07-13 15:35:19 -0300

    merge

--- TpFinalBioApp/views.py
+++ TpFinalBioApp/views.py
@@ -1,16 +1,19 @@
 import json
 import os
+<<<<<<< HEAD
 import re
 from django.core import serializers
 from subprocess import Popen, PIPE
+=======
+import platform
+import subprocess
+>>>>>>> b20dda35eca5a3ebd166dc3b974fd557006cf40e
 
-from Bio.Align.Applications import ClustalwCommandline
 import gmplot
-from Bio import SeqIO, AlignIO, Entrez
+from Bio import SeqIO, AlignIO
+from Bio.Align.Applications import ClustalwCommandline
 from django.contrib import messages
 from django.shortcuts import render, redirect
-from Bio.Alphabet.IUPAC import *
-
 
 from TpFinalBioApp.forms import SecuenceForm
 # Create your views here.
@@ -60,16 +63,22 @@ def uploaded_secuence(request):
             fasta_to_insert.content = fasta['seq']
             fasta_to_insert.length = len(fasta['seq'])
             fasta_to_insert.save()
-
+            
         clustalw_exe = r"C:\Program Files (x86)\ClustalW2\clustalw2.exe"
-        clustalw_cline = ClustalwCommandline(clustalw_exe, infile=path, output='FASTA')
+        clustalw_cline = ClustalwCommandline(clustalw_exe, infile=path, output='CLUSTAL')
         assert os.path.isfile(clustalw_exe), "Clustal W executable missing"
         stdout, stderr = clustalw_cline()
         alignment = AlignIO.read('secuences/secuence.aln', "clustal")
         print('alineamiento \n' + str(alignment))
 
 
+        cmd = ['powershell.exe', '-ExecutionPolicy', 'ByPass', '-File', 'secuences/scripts/armado del arbol.ps1']
+        ec = subprocess.call(cmd)
+        print("Powershell returned: {0:d}".format(ec))
+
+
         messages.success(request, f"El archivo se ha subido correctamente")
+        print(platform.system())
         return render(request, "TpFinalBioApp/uploaded_secuence.html", {'fasta_sequences': fasta_sequences})
     else:
         messages.error(request, f"El archivo no es correcto. " + handler.error_message)
