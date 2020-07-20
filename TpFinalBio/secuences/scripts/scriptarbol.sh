5dbfc1d0a18dbae55aec73b7f888680881697c25
lucio <franciolucio@gmail.com>
2020-07-20 13:17:54 -0300

    areglo vista del mapa

--- TpFinalBio/secuences/scripts/scriptarbol.sh
+++ TpFinalBio/secuences/scripts/scriptarbol.sh
@@ -1,1 +1,1 @@
-C:\iqtree -s "C:\Users\LEGION\OneDrive\TPI - LIDS\Introduccion a la Bioinformatica\tp final\bioInformaticaTpFinal\TpFinalBio\secuences\secuence.fasta_aln.fasta"  -m MFP -bb 1000 -redo
+iqtree -s /home/lucio/Desktop/bioInformaticaTpFinal/TpFinalBio/secuences/secuence.fasta_aln.fasta -m MFP -bb 1000 -redo
